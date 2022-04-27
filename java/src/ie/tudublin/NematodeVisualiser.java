69f462949d9952a9f0d2efffc4d258b4a9d5e6d2
Ciaran McBride <C20457886@mytudublin.ie>
2022-04-27 16:50:55 +0100

    Adding in toString()

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -1,13 +1,32 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
-
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
 public class NematodeVisualiser extends PApplet
 {
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
+	public void printNematodes()
+	{
+		for(Nematode a:nematodes)
+		{
+			System.out.println(a);
+		}
+	}
+
+	public void loadNematodes()
+	{
+		Table NemaTable = loadTable("nematodes.csv", "header");
+
+		for (TableRow n:NemaTable.rows())
+		{
+			Nematode a = new Nematode(n);
+			nematodes.add(a);
+		}
+	}
 
 	public void keyPressed()
 	{		
@@ -26,16 +45,13 @@ public class NematodeVisualiser extends PApplet
 	{
 		colorMode(HSB);
 		background(0);
-		smooth();				
-	}
-	
-
-	public void loadNematodes()
-	{
+		loadNematodes();
+		printNematodes();			
 	}
 
 
 	public void draw()
 	{	
+		background(0);
 	}
 }
